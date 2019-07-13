898fc76dc619b1bed2905c89e7975f7aede7b516
Bishal Shrestha <vsal55555@gmail.com>
2019-07-13 23:19:48 +0545

    Initial Commit

---
+++ 12_shree_git_enum_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  12_shree_git_enum_demo
+//
+//  Created by Bishal on 7/13/19.
+//  Copyright © 2019 Bishal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
